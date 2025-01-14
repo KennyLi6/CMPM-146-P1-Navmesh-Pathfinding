8b73fe2c4a0a4e1ebfa5263f17c6abe04c89bf04
KennyLi6 <kennyli3219@gmail.com>
2025-01-13 22:06:30 -0800

    added code from partner and also made A* bidirectional

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -1,3 +1,5 @@
+import heapq
+
 def find_path (source_point, destination_point, mesh):
 
     """
@@ -14,17 +16,21 @@ def find_path (source_point, destination_point, mesh):
         A list of boxes explored by the algorithm
     """
 
-    path = []
-    boxes = {}
-    # find boxes that contain start and goal points
-    for item in mesh["boxes"]: 
-    # item returns coords for boxes
-        if (within_bounds(item, source_point)):
-            boxes.update({"source": item})
-        if (within_bounds(item, destination_point)):
-            boxes.update({"goal": item})
-    path, boxes = BFS(boxes["source"], boxes["goal"], mesh)
-    return path, boxes.keys()
+    start  = None
+    destination = None
+    for i in mesh["boxes"]: 
+        if (within_bounds(i, source_point)):
+            start = i
+        if (within_bounds(i, destination_point)):
+            destination = i
+        if(start != None and destination != None):
+            path, boxes = A_algorithm(start, destination, mesh)
+            if(len(path)>=2):
+                path[0] = source_point
+                path[len(path) - 1] = destination_point
+            return path, boxes
+        
+    return [],[] ## not in range
 
 def within_bounds (box, point):
     # check if point in range of box 
@@ -36,34 +42,74 @@ def within_bounds (box, point):
         return True
     return False
 
-def BFS (start, goal, mesh):
-    # add start point to queue
-    queue = [start]
-    came_from = dict()
-    came_from[start] = None
+def get_priority(current, destination):
+    x1 = (current[0] + current[1])
+    x2 = (destination[0] + destination[1])
+    y1 = (current[2] + current[3])
+    y2 = (destination[2] + destination[3])
+    return ((x1- x2)**2 + (y1 - y2)**2) ** 0.5
+    ## calculate by heuristic function then get the distance
 
-    # visit boxes
-    while not len(queue) == 0:
-        current = queue.pop()
-        for box in mesh["adj"][current]:
-            if box not in came_from:
-                queue.append(box)
-                came_from[box] = current
-
-    # create path from goal
-    current = goal
+def A_algorithm (start, destination, mesh):
     path = []
-    
-    while current != start:
-        if (came_from[current] == None):
-            print("No path!")
-            path = [(start[0],start[2]),(start[0],start[2])]
-            break
-        path.append(((current[0]+current[1])/2,(current[2]+current[3])/2))
-        current = came_from[current]
-    path.append(((start[0]+start[1])/2,(start[2]+start[3])/2))
-    path.reverse()
-    return path, came_from
+    pqueue = []
+    heapq.heappush(pqueue, (0, start, "destination"))
+    heapq.heappush(pqueue, (0, destination, "start"))
+    visited = [] #save visited box
+    forward_visited = []
+    backward_visited = []
+    if start == destination:
+        return path, visited
+    #initalize tables
+    forward_came_from = {start: None}        
+    forward_cost = {start: 0}
+    backward_came_from = {destination: None}
+    backward_cost = {destination: 0}
+
+    while  pqueue: ## search box
+        e, current, goal = heapq.heappop(pqueue)
+        # find out which search added the node to queue
+        terminate_check = backward_came_from
+        if (goal == "start"):
+            terminate_check = forward_came_from
+        # check if node in other search's list
+        if (current in terminate_check):
+            midpoint = current
+            # if current is in both lists, rebuild path
+            # this loop builds the path from midpoint to start
+            while current is not None:
+                path.append(((current[0] + current[1]) / 2, (current[2] + current[3]) / 2))
+                current = forward_came_from[current]
+            # since the path is reversed, i.e. Midpoint, B, A, start, we have to reverse the list
+            path.reverse() 
+            # reset current to the midpoint
+            current = midpoint
+            # this loops build path from midpoint to goal
+            while current is not None:
+                    path.append(((current[0] + current[1]) / 2, (current[2] + current[3]) / 2))
+                    current = backward_came_from[current]
+            # this one is not reversed so we don't have to modify the list
+            pqueue.clear()
+            return path, visited
+
+        visited.append(current)
+        for i in mesh['adj'][current]:
+            if (goal == "destination"):
+                temporary_cost = forward_cost[current] + get_priority(current,i)
+                if i not in forward_came_from or temporary_cost < forward_cost[i]:
+                    forward_cost[i] = temporary_cost
+                    forward_came_from[i] = current
+                    priority = temporary_cost + get_priority(i, destination)
+                    heapq.heappush(pqueue, (priority, i, goal))
+            if (goal == "start"):
+                temporary_cost = backward_cost[current] + get_priority(current,i)
+                if i not in backward_came_from or temporary_cost < backward_cost[i]:
+                    backward_cost[i] = temporary_cost
+                    backward_came_from[i] = current
+                    priority = temporary_cost + get_priority(i, start)
+                    heapq.heappush(pqueue, (priority, i, goal))
+    print("No path!")
+    return path, visited
             
     
 
